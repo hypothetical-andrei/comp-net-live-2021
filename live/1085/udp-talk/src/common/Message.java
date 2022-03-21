4a35e67e55b6c507d184d825ccc111aef15089ea
AT <hypothetical.andrei@gmail.com>
2022-03-21 11:28:38 +0200

    added message 1085

---
+++ live/1085/udp-talk/src/common/Message.java
@@ -0,0 +1,59 @@
+package common;
+
+import java.io.Serializable;
+import java.net.InetSocketAddress;
+
+public class Message implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	public static final int SUBSCRIBE = 1;
+	public static final int UNSUBSCRIBE = 2;
+	public static final int TALK = 3;
+	
+	private int type;
+	private String text;
+
+	private volatile InetSocketAddress address;
+
+	public Message(int type, String text) {
+		this.type = type;
+		this.text = text;
+	}
+
+	public InetSocketAddress getAddress() {
+		return address;
+	}
+
+	public void setAddress(InetSocketAddress address) {
+		this.address = address;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public static Message subscribe() {
+		return new Message(SUBSCRIBE, null);
+	}
+	
+	public static Message unsubscribe() {
+		return new Message(UNSUBSCRIBE, null);
+	}
+	
+	public static Message talk(String text) {
+		return new Message(TALK, text);
+	}
+	
+}
