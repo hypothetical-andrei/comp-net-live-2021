fe9a740a6ceaee0edd0b95ebb174eca8ad17d82e
AT <hypothetical.andrei@gmail.com>
2022-03-21 10:29:15 +0200

    added server 1092

---
+++ live/1092/udp-talk/src/server/Server.java
@@ -0,0 +1,65 @@
+package server;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.util.HashSet;
+import java.util.Set;
+
+import common.Message;
+import common.Transport;
+
+public class Server implements AutoCloseable {
+
+	private DatagramSocket socket;
+
+	public void start(int port) throws SocketException {
+		stop();
+		final Set<InetSocketAddress> addresses = new HashSet<InetSocketAddress>();
+		socket = new DatagramSocket(port);
+		socket.setSendBufferSize(10 * 1024);
+		new Thread(() -> {
+			while (socket != null && !socket.isClosed()) {
+				try {
+					Message message = Transport.receive(socket);
+					switch (message.getType()) {
+					case Message.SUBSCRIBE:
+						addresses.add(message.getAddress());
+						break;
+					case Message.UNSUBSCRIBE:
+						addresses.remove(message.getAddress());
+						break;
+					case Message.TALK:
+						addresses.forEach(address -> {
+							try {
+								Transport.send(message, socket, address);
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+//								e.printStackTrace();
+							}
+						});
+						break;
+					default:
+						break;
+					}
+				} catch (Exception e) {
+//					System.out.println(e.getMessage());
+				}
+			}
+		}).start();
+	}
+	
+	public void stop() {
+		if (socket != null && !socket.isClosed()) {
+			socket.close();
+			socket = null;
+		}
+	}
+
+	@Override
+	public void close() throws Exception {
+		stop();		
+	}
+
+}
