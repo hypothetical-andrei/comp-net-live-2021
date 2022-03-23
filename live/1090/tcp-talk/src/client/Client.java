4d85f2c8cbb7137cd7c73427c4936acae921d351
AT <hypothetical.andrei@gmail.com>
2022-03-23 17:27:21 +0200

    .

---
+++ live/1090/tcp-talk/src/client/Client.java
@@ -0,0 +1,47 @@
+package client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import common.Transport;
+
+public class Client implements AutoCloseable {
+
+	private Socket socket;
+	
+	public interface ClientCallback {
+		void onTalk(String message);
+	}
+	
+	public Client(String host, int port, ClientCallback callback) throws UnknownHostException, IOException {
+		socket = new Socket(host, port);
+		new Thread(()-> {
+			while (socket != null && !socket.isClosed()) {
+				try {
+					callback.onTalk(Transport.receive(socket));
+				} catch (ClassNotFoundException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+	
+	public void send(String message) {
+		try {
+			Transport.send(message, socket);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void close() throws IOException {
+		if (socket != null && !socket.isClosed()) {
+			socket.close();
+		}
+	}
+
+}
