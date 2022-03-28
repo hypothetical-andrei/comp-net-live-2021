c6ce3425b73fe89b770be15d964854ea16516f25
AT <hypothetical.andrei@gmail.com>
2022-03-28 09:30:30 +0300

    .

---
+++ live/1092/http-basic/src/server/Program.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		try (WebServer server = new WebServer()) {
+			server.open(Settings.PORT);
+			System.out.println("Server is running. Type 'exit' to close");
+			try (Scanner scanner = new Scanner(System.in)) {
+				String command = scanner.nextLine();
+				if (command == null || "exit".equals(command)) {
+					break;
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			System.exit(0);
+		}
+	}
+
+}
---
+++ live/1092/http-basic/src/server/WebServer.java
@@ -0,0 +1,11 @@
+package server;
+
+public class WebServer implements AutoCloseable{
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
