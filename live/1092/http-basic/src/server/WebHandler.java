b1c09577fe1451634f05ffa94cd382bbeb160b1b
AT <hypothetical.andrei@gmail.com>
2022-03-28 09:59:39 +0300

    .

--- live/1092/http-basic/src/server/WebHandler.java
+++ live/1092/http-basic/src/server/WebHandler.java
@@ -1,6 +1,9 @@
 package server;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -9,8 +12,28 @@ public class WebHandler implements HttpHandler {
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		WebRequest request = new WebRequest(exchange);
-
+		WebRequest request;
+		try {
+			request = new WebRequest(exchange);
+			Path path = Paths.get("web", request.getPath());
+			
+			if (path.toFile().exists()) {
+				if (path.toFile().isDirectory()) {
+					exchange.sendResponseHeaders(403, 0);
+				} else {
+					byte[] bytes = Files.readAllBytes(path);
+					exchange.sendResponseHeaders(200, bytes.length);
+					exchange.getResponseBody().write(bytes, 0, bytes.length);
+				}
+			} else {
+				exchange.sendResponseHeaders(404, 0);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			exchange.sendResponseHeaders(500, 0);
+		} finally {
+			exchange.close();
+		}
 	}
 
 }
