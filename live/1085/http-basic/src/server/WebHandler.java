20cef72911f01e93f290a0bdcc5a5ba7573f6359
AT <hypothetical.andrei@gmail.com>
2022-03-28 11:36:26 +0300

    .

--- live/1085/http-basic/src/server/WebHandler.java
+++ live/1085/http-basic/src/server/WebHandler.java
@@ -1,6 +1,9 @@
 package server;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -9,7 +12,27 @@ public class WebHandler implements HttpHandler {
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		WebRequest request = new WebRequest(exchange);
+		WebRequest request;
+		try {
+			request = new WebRequest(exchange);
+			Path path = Paths.get("web", request.getPath());
+			if (path.toFile().exists()) {
+				if (path.toFile().isDirectory()) {
+					exchange.sendResponseHeaders(403, 0);
+				} else {
+					byte[] buffer = Files.readAllBytes(path);
+					exchange.sendResponseHeaders(200, buffer.length);
+					exchange.getResponseBody().write(buffer, 0, buffer.length);
+				}
+			} else {
+				exchange.sendResponseHeaders(404, 0);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			exchange.sendResponseHeaders(500, 0);
+		} finally {
+			exchange.close();
+		}
 	}
 
 }
--- live/1085/http-basic/src/server/WebResponse.java
+++ live/1085/http-basic/src/server/WebResponse.java
@@ -1,5 +1,33 @@
 package server;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+
 public class WebResponse {
 
+	private int status = 200;
+	private String contentType = "text/plain";
+	private ByteArrayOutputStream stream = new ByteArrayOutputStream();
+	private PrintWriter writer = new PrintWriter(stream);
+	
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getContentType() {
+		return contentType;
+	}
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+	public ByteArrayOutputStream getStream() {
+		return stream;
+	}
+	public PrintWriter getWriter() {
+		return writer;
+	}
+	
+	
 }
