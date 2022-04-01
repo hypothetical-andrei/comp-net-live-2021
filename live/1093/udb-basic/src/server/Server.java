1bec0eff2be1989e4ae3ea5ca6a32c171e599458
AT <hypothetical.andrei@gmail.com>
2022-04-01 13:29:59 +0300

    .

--- live/1093/udb-basic/src/server/Server.java
+++ live/1093/udb-basic/src/server/Server.java
@@ -1,15 +1,37 @@
 package server;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Server implements AutoCloseable{
 
-	public Server(int port) {
-		// TODO Auto-generated constructor stub
+	private DatagramSocket socket;
+	
+	public Server(int port) throws IOException {
+		socket = new DatagramSocket(port);
+		while (socket != null && !socket.isClosed()) {
+			byte[] buffer = new byte[512];
+			DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+			socket.receive(request);
+			InetAddress clientAddress = request.getAddress();
+			int clientPort = request.getPort();
+			
+			String decoded = new String(buffer, 0, request.getLength());
+			buffer = decoded.toUpperCase().getBytes();
+			
+			DatagramPacket response = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
+			socket.send(response);
+		}
 	}
 
 	@Override
 	public void close() throws Exception {
-		// TODO Auto-generated method stub
-		
+		if (socket != null && !socket.isClosed()) {
+			socket.close();
+			socket = null;
+		}
 	}
 
 }
