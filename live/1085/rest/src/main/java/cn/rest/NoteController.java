1a30e0282d8a7ca9162815dfa2c7ec8a982d6b12
AT <hypothetical.andrei@gmail.com>
2022-04-04 11:32:51 +0300

    .

---
+++ live/1085/rest/src/main/java/cn/rest/NoteController.java
@@ -0,0 +1,23 @@
+package cn.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+
+	@Autowired
+	private NoteRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Note>> getNotes() {
+		List<Note> notes = (List<Note>) repository.findAll();
+		return notes.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(notes);
+	}
+}
---
+++ live/1085/rest/src/main/java/cn/rest/NoteRepository.java
@@ -0,0 +1,9 @@
+package cn.rest;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends PagingAndSortingRepository<Note, Long>{
+
+}
