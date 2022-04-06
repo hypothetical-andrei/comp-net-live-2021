4c556a662a21e170d59bf2f869bb4fc76f37edc8
AT <hypothetical.andrei@gmail.com>
2022-04-06 17:03:00 +0300

    .

--- live/1090/rest/src/main/java/ro/ase/rest/NoteController.java
+++ live/1090/rest/src/main/java/ro/ase/rest/NoteController.java
@@ -1,14 +1,21 @@
 package ro.ase.rest;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping("/api/notes")
@@ -18,14 +25,39 @@ public class NoteController {
 	private NoteRepository repository;
 	
 	@GetMapping
-	private ResponseEntity<List<Note>> getNotes() {
+	public ResponseEntity<List<Note>> getNotes() {
 		List<Note> notes = (List<Note>) repository.findAll();
 		return notes.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(notes);
 	}
 	
 	@GetMapping("/{id}")
-	private ResponseEntity<Note> getNote(@PathVariable long id) {
+	public ResponseEntity<Note> getNote(@PathVariable long id) {
 		Optional<Note> note = repository.findById(Long.valueOf(id));
 		return note.isPresent() ? ResponseEntity.ok(note.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Note> addNote(@RequestBody Note note) {
+		try {
+			Note createdNote = repository.save(note);
+			URI uri = linkTo(NoteController.class).slash(note.getId()).toUri();
+			return ResponseEntity.created(uri).build();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> changeNote(@PathVariable long id, @RequestBody Note note) {
+		Optional<Note> existingNoteOptional = repository.findById(Long.valueOf(id));
+		if (existingNoteOptional.isPresent()) {
+			Note existingNote = existingNoteOptional.get();
+			existingNote.setTitle(note.getTitle());
+			existingNote.setContent(note.getContent());
+			repository.save(existingNote);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
