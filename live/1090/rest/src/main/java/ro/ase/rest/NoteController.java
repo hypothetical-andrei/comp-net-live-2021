aeb0cf98c82df9edde4d3ccd72282dbe9917a701
AT <hypothetical.andrei@gmail.com>
2022-04-06 17:33:22 +0300

    .

--- live/1090/rest/src/main/java/ro/ase/rest/NoteController.java
+++ live/1090/rest/src/main/java/ro/ase/rest/NoteController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
@@ -24,18 +31,32 @@ public class NoteController {
 	@Autowired
 	private NoteRepository repository;
 	
+	@Operation(summary = "Get all notes", operationId = "getNotes")
+    @ApiResponses({ 
+            @ApiResponse(responseCode = "200", description = "Found notes", 
+                content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Note[].class))}),
+            @ApiResponse(responseCode = "204", description = "No notes found", content = @Content)})	
 	@GetMapping
 	public ResponseEntity<List<Note>> getNotes() {
 		List<Note> notes = (List<Note>) repository.findAll();
 		return notes.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(notes);
 	}
 	
+	@Operation(summary = "Get a note by its id", operationId = "getNote")
+    @ApiResponses({ 
+            @ApiResponse(responseCode = "200", description = "Found the note",
+            	content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Note.class))}),
+            @ApiResponse(responseCode = "404", description = "Note not found", content = @Content)})
 	@GetMapping("/{id}")
 	public ResponseEntity<Note> getNote(@PathVariable long id) {
 		Optional<Note> note = repository.findById(Long.valueOf(id));
 		return note.isPresent() ? ResponseEntity.ok(note.get()) : ResponseEntity.notFound().build();
 	}
 	
+	@Operation(summary = "Create a new note", operationId = "addNote")
+    @ApiResponses({ 
+            @ApiResponse(responseCode = "201", description = "Note was created", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content)})
 	@PostMapping
 	public ResponseEntity<Note> addNote(@RequestBody Note note) {
 		try {
@@ -47,6 +68,12 @@ public class NoteController {
 		}
 	}
 	
+	
+	@Operation(summary = "Update a note by its id", operationId = "changeNote")
+    @ApiResponses({ 
+            @ApiResponse(responseCode = "204", description = "Note was update", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Note not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content)})
 	@PutMapping("/{id}")
 	public ResponseEntity<Void> changeNote(@PathVariable long id, @RequestBody Note note) {
 		Optional<Note> existingNoteOptional = repository.findById(Long.valueOf(id));
@@ -55,6 +82,21 @@ public class NoteController {
 			existingNote.setTitle(note.getTitle());
 			existingNote.setContent(note.getContent());
 			repository.save(existingNote);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@Operation(summary = "Delete a note by its id", operationId = "removeNote")
+    @ApiResponses({ 
+            @ApiResponse(responseCode = "204", description = "Note was deleted", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Note not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content)})
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> removeNote(@PathVariable long id) {
+		if (repository.existsById(Long.valueOf(id))) {
+			repository.deleteById(Long.valueOf(id));
 			return ResponseEntity.noContent().build();
 		} else {
 			return ResponseEntity.notFound().build();
--- live/1090/rest/src/main/java/ro/ase/rest/RestApplication.java
+++ live/1090/rest/src/main/java/ro/ase/rest/RestApplication.java
@@ -1,7 +1,13 @@
 package ro.ase.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class RestApplication {
@@ -10,4 +16,12 @@ public class RestApplication {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI openApi(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI().info(new Info().title("Awesome notes API")
+				.version(appVersion)
+				.description("This is a sample Notes API")
+				.termsOfService("http://swagger.io/terms/")
+				.license(new License().name("GPLv3").url("https://www.gnu.org/licenses/gpl-3.0.en.html")));
+	}
 }
