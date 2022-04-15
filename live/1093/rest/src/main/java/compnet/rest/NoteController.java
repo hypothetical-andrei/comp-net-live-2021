e58b872234147734253fee76c7d538a525ebe65e
AT <hypothetical.andrei@gmail.com>
2022-04-15 13:38:25 +0300

    .

--- live/1093/rest/src/main/java/compnet/rest/NoteController.java
+++ live/1093/rest/src/main/java/compnet/rest/NoteController.java
@@ -7,9 +7,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +46,40 @@ public class NoteController {
 		} catch (Exception e) {
 			e.printStackTrace();
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> changeNote(@PathVariable long id, @RequestBody Note note) {
+		Optional<Note> existingNoteOptional = repository.findById(Long.valueOf(id));
+		if (existingNoteOptional.isPresent()) {
+			try {
+				Note existingNote = existingNoteOptional.get();
+				existingNote.setTitle(note.getTitle());
+				existingNote.setContent(note.getContent());
+				repository.save(existingNote);
+				return ResponseEntity.noContent().build();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+			}
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> removeNote(@PathVariable long id) {
+		if (repository.existsById(Long.valueOf(id))) {
+			try {
+				repository.deleteById(Long.valueOf(id));
+				return ResponseEntity.noContent().build();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+			}
+		} else {
+			return ResponseEntity.notFound().build();
 		}
 	}
 }
