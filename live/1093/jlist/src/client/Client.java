2ec842b7b29000fdbfa9953fd1755e6f071406b7
AT <hypothetical.andrei@gmail.com>
2022-04-29 13:33:52 +0300

    .

---
+++ live/1093/jlist/src/client/Client.java
@@ -0,0 +1,37 @@
+package client;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import common.Transport;
+
+public class Client implements AutoCloseable{
+
+	public interface ClientCallback {
+		void onReceive(String message);
+	}
+	
+	private Socket socket;
+	
+	public Client(String host, int port, ClientCallback callback) {
+		socket = new Socket(host, port);
+		new Thread(() -> {
+			while (socket != null && !socket.isClosed()) {
+				try {
+					callback.onReceive(Transport.receive(socket));
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+	
+	@Override
+	public void close() throws Exception {
+		if (socket != null && !socket.isClosed()) {
+			socket.close();
+		}
+	}
+
+}
---
+++ live/1093/jlist/src/client/Program.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.util.Scanner;
+
+import common.Settings;
+
+public class Program {
+
+	public static void main(String[] args) {
+		try (Client client = new Client(Settings.HOST, Settings.PORT, message -> {
+			System.out.println(message);
+		})) {
+			System.out.println("Client started. Type 'exit' to close.");
+			try (Scanner scanner = new Scanner(System.in)) {
+				while (true) {
+					String command = scanner.nextLine();
+					if (command == null || "exit".equals(command)) {
+						break;
+					}
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			System.exit(0);
+		}
+	}
+
+}
