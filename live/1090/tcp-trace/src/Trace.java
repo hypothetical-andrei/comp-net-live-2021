354b075cbf49bc3b4d5a06b678c7b33cffe411e4
AT <hypothetical.andrei@gmail.com>
2022-05-04 16:52:03 +0300

    .

---
+++ live/1090/tcp-trace/src/Trace.java
@@ -0,0 +1,169 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+
+public class Trace extends Shell {
+	private Text sourcePort;
+	private Text destinationPort;
+	private ServerSocket serverSocket;
+	private ExecutorService executorService = Executors.newFixedThreadPool(10 * Runtime.getRuntime().availableProcessors());
+	private List list;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		try {
+			Display display = Display.getDefault();
+			Trace shell = new Trace(display);
+			shell.open();
+			shell.layout();
+			while (!shell.isDisposed()) {
+				if (!display.readAndDispatch()) {
+					display.sleep();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the shell.
+	 * @param display
+	 */
+	public Trace(Display display) {
+		super(display, SWT.SHELL_TRIM);
+		setLayout(new GridLayout(2, false));
+		
+		Label lblNewLabel = new Label(this, SWT.NONE);
+		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblNewLabel.setText("Listen to:");
+		
+		sourcePort = new Text(this, SWT.BORDER);
+		sourcePort.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblNewLabel_1 = new Label(this, SWT.NONE);
+		lblNewLabel_1.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblNewLabel_1.setText("Forward to:");
+		
+		destinationPort = new Text(this, SWT.BORDER);
+		destinationPort.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Button btnNewButton = new Button(this, SWT.NONE);
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				sourcePort.setEnabled(false);
+				destinationPort.setEnabled(false);
+				try {
+					serverSocket = new ServerSocket(Integer.parseInt(sourcePort.getText()));
+					String host = "127.0.0.1";
+					int port = Integer.parseInt(destinationPort.getText());
+					executorService.execute(() -> {
+						while (serverSocket != null && !serverSocket.isClosed()) {
+							try {
+								Socket inboundSocket = serverSocket.accept();
+								Socket outboundSocket = new Socket(host, port);
+								executorService.submit(() -> {
+									forward(inboundSocket, outboundSocket, 0);
+								});
+								;
+								executorService.submit(() -> {
+									forward(outboundSocket, inboundSocket, 1);
+								});
+							} catch (IOException e1) {
+								e1.printStackTrace();
+							}
+						}
+					});
+				} catch (NumberFormatException | IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnNewButton.setText("Start");
+		
+		Button btnNewButton_1 = new Button(this, SWT.NONE);
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				if (serverSocket != null && !serverSocket.isClosed()) {
+					try {
+						serverSocket.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		btnNewButton_1.setText("Stop");
+		new Label(this, SWT.NONE);
+		
+		list = new List(this, SWT.BORDER);
+		GridData gd_list = new GridData(SWT.LEFT, SWT.FILL, false, false, 1, 1);
+		gd_list.heightHint = 355;
+		gd_list.widthHint = 797;
+		list.setLayoutData(gd_list);
+		createContents();
+	}
+
+	/**
+	 * Create contents of the shell.
+	 */
+	protected void createContents() {
+		setText("SWT Application");
+		setSize(916, 515);
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+	
+	private void forward(Socket inboundSocket, Socket outboundSocket, int direction) {
+		while (inboundSocket != null && !inboundSocket.isClosed()) {
+			try {
+				int available = inboundSocket.getInputStream().available();
+				if (available > 0) {
+					byte[] buffer = new byte[available];
+					int count = 0;
+					while (count < buffer.length) {
+						count += inboundSocket.getInputStream().read(buffer, count, buffer.length);
+						Display.getDefault().asyncExec(() -> {
+							list.add(new String(buffer));
+							list.redraw();
+						});
+						if (outboundSocket != null && !outboundSocket.isClosed()) {
+							outboundSocket.getOutputStream().write(buffer, 0, buffer.length);
+							outboundSocket.getOutputStream().flush();
+						}
+					}
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+}
