73c4787ac659c1aea4d1a43f5ad4991f29caceae
AT <hypothetical.andrei@gmail.com>
2022-05-09 10:02:05 +0300

    .

--- live/1092/tcp-trace/src/Trace.java
+++ live/1092/tcp-trace/src/Trace.java
@@ -22,10 +22,13 @@ public class Trace extends Shell {
 	private List list;
 	private Button btnNewButton;
 	private ServerSocket serverSocket;
-	private ExecutorService executorService = Executors.newFixedThreadPool(10 * Runtime.getRuntime().availableProcessors());
+	private ExecutorService executorService = Executors
+			.newFixedThreadPool(10 * Runtime.getRuntime().availableProcessors());
+	private Button btnNewButton_1;
 
 	/**
 	 * Launch the application.
+	 * 
 	 * @param args
 	 */
 	public static void main(String args[]) {
@@ -46,26 +49,27 @@ public class Trace extends Shell {
 
 	/**
 	 * Create the shell.
+	 * 
 	 * @param display
 	 */
 	public Trace(Display display) {
 		super(display, SWT.SHELL_TRIM);
 		setLayout(new GridLayout(2, false));
-		
+
 		Label lblNewLabel = new Label(this, SWT.NONE);
 		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblNewLabel.setText("Source port");
-		
+
 		sourcePort = new Text(this, SWT.BORDER);
 		sourcePort.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
+
 		Label lblNewLabel_1 = new Label(this, SWT.NONE);
 		lblNewLabel_1.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblNewLabel_1.setText("Destination port");
-		
+
 		destinationPort = new Text(this, SWT.BORDER);
 		destinationPort.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
+
 		btnNewButton = new Button(this, SWT.NONE);
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
@@ -96,16 +100,38 @@ public class Trace extends Shell {
 				}
 			}
 		});
-		GridData gd_btnNewButton = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		GridData gd_btnNewButton = new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1);
 		gd_btnNewButton.widthHint = 140;
 		btnNewButton.setLayoutData(gd_btnNewButton);
 		btnNewButton.setText("Capture");
-		
+
 		list = new List(this, SWT.BORDER);
 		GridData gd_list = new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1);
-		gd_list.heightHint = 396;
+		gd_list.heightHint = 369;
 		gd_list.widthHint = 801;
 		list.setLayoutData(gd_list);
+
+		btnNewButton_1 = new Button(this, SWT.NONE);
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				if (serverSocket != null && !serverSocket.isClosed()) {
+					try {
+						serverSocket.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				sourcePort.setEnabled(true);
+				destinationPort.setEnabled(true);
+			}
+		});
+		GridData gd_btnNewButton_1 = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_btnNewButton_1.widthHint = 140;
+		btnNewButton_1.setLayoutData(gd_btnNewButton_1);
+		btnNewButton_1.setText("Stop");
+		new Label(this, SWT.NONE);
 		createContents();
 	}
 
@@ -114,13 +140,39 @@ public class Trace extends Shell {
 	 */
 	protected void createContents() {
 		setText("SWT Application");
-		setSize(965, 511);
+		setSize(965, 515);
 
 	}
 
 	@Override
 	protected void checkSubclass() {
 		// Disable the check that prevents subclassing of SWT components
+	}
+
+	private void forward(Socket inboundSocket, Socket outboundSocket) {
+		try {
+			while (inboundSocket != null && !inboundSocket.isClosed()) {
+				int available = inboundSocket.getInputStream().available();
+				if (available > 0) {
+					byte[] buffer = new byte[available];
+					int count = 0;
+					while (count < buffer.length) {
+						count += inboundSocket.getInputStream().read(buffer, count, buffer.length);
+					}
+					Display.getDefault().asyncExec(() -> {
+						list.add(new String(buffer));
+						list.redraw();
+					});
+					if (outboundSocket != null && !outboundSocket.isClosed()) {
+						outboundSocket.getOutputStream().write(buffer, 0, buffer.length);
+						outboundSocket.getOutputStream().flush();
+					}
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
